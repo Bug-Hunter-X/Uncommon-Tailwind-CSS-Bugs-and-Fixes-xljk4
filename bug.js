f2ee614b22c5788e5bc1d4290cb87aaf435374d0
BugHunter-X <askzenai@gmail.com>
2024-12-26 18:34:07 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,8 @@
+```javascript
+// ... your Tailwind CSS classes ...
+<div class="bg-gray-100 p-4 rounded-lg shadow-md">
+  <p class="text-lg font-medium">This is some text.</p>
+</div>
+```
+
+This code might cause unexpected issues if you don't have the correct Tailwind directives in your CSS file or if your config file has issues.
